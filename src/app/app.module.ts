b2831cc05e56127a1c0db28178f6c2335a518ff3
fukkong <rivirtu@gmail.com>
2019-10-17 23:27:14 +0900

    add momnetjs add component for test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimeComponent } from './time/time.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/time/time.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import * as moment from 'moment';
+
+@Component({
+  selector: 'app-time',
+  templateUrl: './time.component.html',
+  styleUrls: ['./time.component.css']
+})
+export class TimeComponent implements OnInit {
+  nowDate = '';
+  nowTime = '';
+  now = moment();
+
+  constructor() {
+    this.nowDate = moment(new Date()).format('YYYY.MM.DD');
+    this.nowTime = moment(new Date()).format('LT');
+    console.log(this.now);
+  }
+
+  ngOnInit() {
+  }
+
+}
