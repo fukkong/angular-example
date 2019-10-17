e1e40357a69aee52993796f7a5475a5a532b61d5
fukkong <rivirtu@gmail.com>
2019-10-18 00:58:14 +0900

    make ko.ts and apply to datePipe locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {registerLocaleData} from '@angular/common';
+import localeKo from './time/ko';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TimeComponent } from './time/time.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {TimeComponent} from './time/time.component';
+
+registerLocaleData(localeKo, 'ko');
 
 @NgModule({
   declarations: [
@@ -17,4 +21,5 @@ import { TimeComponent } from './time/time.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
