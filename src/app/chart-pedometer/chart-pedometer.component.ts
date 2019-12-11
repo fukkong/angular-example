bc4911d77231647370c7f03cb4a3fdd18bccc8ac
fukkong <rivirtu@gmail.com>
2019-12-11 12:30:19 +0900

    refactor bpChart

--- src/app/chart-pedometer/chart-pedometer.component.ts
+++ src/app/chart-pedometer/chart-pedometer.component.ts
@@ -1,19 +1,21 @@
 import {Component, OnInit} from '@angular/core';
-
 import * as Highcharts from 'highcharts';
 import Boost from 'highcharts/modules/boost';
 import noData from 'highcharts/modules/no-data-to-display';
 import More from 'highcharts/highcharts-more';
 import * as moment from 'moment';
-import {PedometerDaySummary, PedometerTimeSegment} from 'AutochekCommon/vanilla/objects/device-data-object';
+import MomentTimeZone from 'moment-timezone';
+import {GlucosemeterDaySummary, PedometerDaySummary, PedometerTimeSegment} from 'AutochekCommon/vanilla/objects/device-data-object';
 import {drawPedometerChart} from 'AutochekChart/pedometer-chart';
+import {drawGlucoseChart} from 'AutochekChart/glucose-chart';
+import {AutochekChartOption} from 'AutochekChart/chart.option';
 
 Boost(Highcharts);
 noData(Highcharts);
 More(Highcharts);
 
-//window['moment'] = moment;
-//MomentTimeZone();
+window['moment'] = moment;
+MomentTimeZone();
 
 @Component({
   selector: 'app-chart-pedometer',
@@ -26,25 +28,88 @@ export class ChartPedometerComponent implements OnInit {
   }
 
   ngOnInit() {
-    const data = makeTestCase();
-    drawPedometerChart('container', data);
+    const end = moment().startOf('day').toDate();
+    const startOfDay = moment(end).subtract(1, 'day').toDate();
+    const start = moment(end).subtract(31, 'day').toDate();
+    const data = makeTestCase(start, end);
+    // const data = makeTestCaseforDayData(startOfDay, end);
+    drawPedometerChart('container', data, {start, end});
+    const glucoseData: GlucosemeterDaySummary[] = makeTestCaseForGlucose(start, end);
+    const glucoseMinMax: AutochekChartOption = {
+      glucose: {
+        b_meal_min: 80,
+        b_meal_max: 120,
+        a_meal_min: 100,
+        a_meal_max: 200,
+        sleep_min: 80,
+        sleep_max: 120
+      }
+    };
+    drawGlucoseChart('glucose', glucoseData, glucoseMinMax);
   }
 }
 
-function makeTestCase() {
-  let startOfWeek = moment().startOf('week').toDate();
-  const now = moment().startOf('day');
-  const diff = now.diff(startOfWeek, 'days');
+function makeTestCase(start, end) {
+  const diff = moment(end).diff(start, 'days');
 
   const rtnPedometerList: PedometerDaySummary[] = [];
 
   for (let i = 0; i < diff + 1; i++) {
-    startOfWeek = moment(startOfWeek).add(1, 'day').utc();
+    const temp = moment(start).add(i, 'day').toDate();
     const step = Math.floor(Math.random() * 4000);
     const cal = Number.parseFloat((Math.random() * 100).toFixed(2));
     const dist = Number.parseFloat((Math.random() * 4).toFixed(2));
-    const tempPedometer = new PedometerDaySummary(startOfWeek, step, cal, dist);
+    const tempPedometer = new PedometerDaySummary(temp, step, cal, dist);
     rtnPedometerList.push(tempPedometer);
   }
   return rtnPedometerList;
+}
+
+function makeTestCaseforDayData(start, end) {
+  const diff = moment(end).diff(start, 'second') / 300;
+
+  const rtnPedometerTimeSegment: PedometerTimeSegment[] = [];
+
+  for (let i = 0; i < diff; i++) {
+    const temp = moment(start).add(5 * i, 'minute').toDate();
+    const step = Math.floor(Math.random() * 1000);
+    const cal = Number.parseFloat((Math.random() * 50).toFixed(2));
+    const dist = Number.parseFloat((Math.random() * 2).toFixed(2));
+    const tempPedometer = new PedometerTimeSegment(temp, 10, step, cal, dist);
+    rtnPedometerTimeSegment.push(tempPedometer);
+  }
+  return rtnPedometerTimeSegment;
+}
+
+function makeTestCaseForGlucose(start, end) {
+  const diff = moment(end).diff(start, 'day');
+
+  const rtnGlucoseDaySummery: GlucosemeterDaySummary[] = [];
+
+  for (let i = 0; i < diff; i++) {
+    const temp = moment(start).add(i, 'day').toDate();
+    const bBreakfast = Number.parseFloat((Math.random() * 140 + 50).toFixed(2));
+    const aBreakfast = Number.parseFloat((Math.random() * 160 + 50).toFixed(2));
+    // const bLunch = Number.parseFloat((Math.random() * 140 + 50).toFixed(2));
+    const aLunch = Number.parseFloat((Math.random() * 140 + 50).toFixed(2));
+    const bDinner = Number.parseFloat((Math.random() * 140 + 50).toFixed(2));
+    // const aDinner = Number.parseFloat((Math.random() * 140 + 50).toFixed(2));
+    const bSleep = Number.parseFloat((Math.random() * 140 + 50).toFixed(2));
+
+    const tempGlucose = new GlucosemeterDaySummary(temp);
+
+    tempGlucose.morningBeforeMeal = bBreakfast;
+    tempGlucose.morningAfterMeal = aBreakfast;
+    // tempGlucose.afternoonBeforeMeal = bLunch;
+    tempGlucose.afternoonAfterMeal = aLunch;
+    tempGlucose.eveningBeforeMeal = bDinner;
+    // tempGlucose.eveningAfterMeal = aDinner;
+    if (bSleep > 170) {
+      tempGlucose.beforeSleep = bSleep;
+    }
+
+    rtnGlucoseDaySummery.push(tempGlucose);
+  }
+
+  return rtnGlucoseDaySummery;
 }
--- src/app/highchart/highchart.component.ts
+++ src/app/highchart/highchart.component.ts
@@ -7,7 +7,9 @@ import xrange from 'highcharts/modules/xrange';
 import * as moment from 'moment';
 import MomentTimeZone from 'moment-timezone';
 import {drawSleepChart} from '../../../AutochekChart/sleep-segment-chart';
-import {PedometerSleepSegment} from '@AutochekCommon/vanilla/objects/device-data-object';
+import {PedometerHeartrateSegment, PedometerSleepSegment} from '@AutochekCommon/vanilla/objects/device-data-object';
+import {drawHeartRateChart} from '@AutochekChart/heart-rate-chart';
+import {drawBloodpressurePeriodChart} from '@AutochekChart/bpchart';
 
 Boost(Highcharts);
 noData(Highcharts);
@@ -28,210 +30,94 @@ export class HighchartComponent implements OnInit {
   }
 
   ngOnInit() {
-    Highcharts.setOptions({
-      lang: {
-        months: [
-          '1', '2', '3', '4',
-          '5', '6', '7', '8',
-          '9', '10', '11', '12'
-        ],
-        weekdays: [
-          '일', '월', '화', '수', '목', '금', '토'
-        ],
-        shortMonths: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
-      }
-    });
-
-    const start = moment().startOf('day').toDate();
-    const end = moment().endOf('day').toDate();
-    drawPeriodChart('container');
+    const start = moment('2019-10-25T04:10:51.530Z').startOf('day').toDate();
+    const end = moment('2019-10-25T12:15:51.530Z').endOf('day').toDate();
+    drawBloodpressurePeriodChart('container2', bloodPressureData, {start: start, end: end});
     const testCase = makeTestCase();
     drawSleepChart('sleepLog', testCase);
-    drawHeartRateChart('heartRate');
+    drawHeartRateChart('heartRate', heartRateData).then(() => {
+    });
   }
 }
 
 function makeTestCase() {
-  const startTime = moment().startOf('day');
   const addTime = Math.floor(Math.random() * 3 + 5);
   const rtnPedometerList: PedometerSleepSegment[] = [];
   let tempTime: Date = moment().startOf('day').toDate();
   for (let i = 0; i < addTime * 6; i++) {
     tempTime = new Date(moment(tempTime).add(10, 'minute').toDate());
-    const tempSleepIndex = Math.floor(Math.random() * 3);
+    const tempSleepIndex = Math.floor(Math.random() * 3 + 1);
     const tempPedometer = new PedometerSleepSegment(tempTime, tempSleepIndex);
     rtnPedometerList.push(tempPedometer);
   }
   return rtnPedometerList;
 }
 
-function setBloodPressureOption(bpData: any, start?: Date, end?: Date) {
-  const options: any = {
-    title: {
-      text: 'Blood Pressure chart'
-    },
-    credits: {
-      enabled: false
-    },
-    tooltip: {
-      shared: true
-    },
-    time: {
-      timezone: 'Asia/Seoul'
-    },
-    xAxis: {
-      type: 'datetime',
-      gridLineWidth: 1,
-      title: {
-        text: '시간'
-      }
-    },
-    yAxis: [{
-      min: 60,
-      max: 220,
-      startOnTick: false,
-      endOnTick: false,
-      labels: {
-        format: '{value}mmHg',
-        style: {
-          color: Highcharts.getOptions().colors[3]
-        }
-      }
-    }, {
-      gridLineWidth: 1,
-      opposite: true,
-      title: {
-        text: '맥박',
-      },
-      labels: {
-        format: '{value}회/분',
-        align: 'left',
-      }
-    }],
-    plotOptions: {
-      series: {
-        marker: {
-          enabled: true
-        },
-        pointWidth: 30
-      }
-    },
-    series: [{
-      name: '수축기',
-      type: 'arearange',
-      lineWidth: 1,
-      fillOpacity: 0.3,
-      zIndex: 0,
-      color: Highcharts.getOptions().colors[3],
-      data: []
-    }, {
-      name: '평균 혈압',
-      zIndex: 1,
-      data: [],
-    }, {
-      name: '심박수',
-      type: 'column',
-      yAxis: 1,
-      zIndex: 0,
-      opacity: 0.8,
-      data: []
-    }]
-  };
-
-  if (start) {
-    options.xAxis.min = start.getTime();
-  }
-  if (end) {
-    options.xAxis.max = end.getTime();
-  }
-
-  const updatedSystolic = [];
-  const updatedAverage = [];
-  const updatedRate = [];
-
-  bpData.forEach(data => {
-    const time = new Date(data.date).getTime();
-    const tempSystolic = [
-      time, data.diastolic, data.systolic
-    ];
-    const tempAverage = [
-      time, data.mean
-    ];
-    const tempRate = [
-      time, data.rate
-    ];
-    updatedSystolic.push(tempSystolic);
-    updatedAverage.push(tempAverage);
-    updatedRate.push(tempRate);
-  });
-
-  options.series[0].data = updatedSystolic;
-  options.series[1].data = updatedAverage;
-  options.series[2].data = updatedRate;
-
-  return options;
-}
-
-async function setHeartRateChartOption(heartRateData: any) {
-  const options: any = {
-    title: {
-      text: 'Heart Rate Chart'
-    },
-    time: {
-      timezone: 'Asia/Seoul'
-    },
-    xAxis: {
-      type: 'datetime',
-      gridLineWidth: 1,
-    },
-    yAxis: {
-      labels: {
-        format: '{value}bpm',
-      }
-    },
-    plotOptions: {},
-    series: [{
-      name: '심박수',
-      type: 'spline',
-      color: Highcharts.getOptions().colors[0],
-      data: []
-    }]
-  };
-
-  heartRateData.forEach(data => {
-    const time = new Date(data.date).getTime();
-    options.series[0].data.push([time, data.rate]);
-  });
-
-  const startOfDay = moment(heartRateData[0].date).startOf('day').toDate()
-  const endOfDay = moment(heartRateData[heartRateData.length - 1].date).endOf('day').toDate()
-
-  options.xAxis.min = startOfDay.getTime();
-  options.xAxis.max = endOfDay.getTime();
-
-  return options;
-}
-
-function drawPeriodChart(canvas: string, start?: Date, end?: Date) {
-  // data query with start and end
-  const bpData = bloodPressureData; // temporary set data as bloodPressureData
-  // set option
-  const option = setBloodPressureOption(bpData, start, end);
-  // draw chart
-  Highcharts.chart(canvas, option);
-}
-
-
-
-async function drawHeartRateChart(canvas: string) {
-  const option = await setHeartRateChartOption(bloodPressureData);
-  await Highcharts.chart(canvas, option);
-}
-
-
-
-
-
+const heartRateData: PedometerHeartrateSegment = [
+  {
+    'date': '2019-10-25T04:10:51.530Z',
+    'systolic': 100,
+    'diastolic': 80,
+    'mean': 80,
+    'rate': 80
+  },
+  {
+    'date': '2019-10-25T04:18:51.530Z',
+    'systolic': 110,
+    'diastolic': 62,
+    'mean': 95,
+    'rate': 120
+  },
+  {
+    'date': '2019-10-25T06:10:51.530Z',
+    'systolic': 110,
+    'diastolic': 65,
+    'mean': 95,
+    'rate': 120
+  },
+  {
+    'date': '2019-10-25T07:10:51.530Z',
+    'systolic': 110,
+    'diastolic': 70,
+    'mean': 95,
+    'rate': 120
+  },
+  {
+    'date': '2019-10-25T08:10:51.530Z',
+    'systolic': 110,
+    'diastolic': 82,
+    'mean': 95,
+    'rate': 120
+  },
+  {
+    'date': '2019-10-25T09:10:51.530Z',
+    'systolic': 110,
+    'diastolic': 80,
+    'mean': 95,
+    'rate': 120
+  },
+  {
+    'date': '2019-10-25T10:10:51.530Z',
+    'systolic': 110,
+    'diastolic': 80,
+    'mean': 95,
+    'rate': 120
+  },
+  {
+    'date': '2019-10-25T11:10:51.530Z',
+    'systolic': 110,
+    'diastolic': 80,
+    'mean': 95,
+    'rate': 120
+  },
+  {
+    'date': '2019-10-25T12:15:51.530Z',
+    'systolic': 110,
+    'diastolic': 80,
+    'mean': 95,
+    'rate': 120
+  },
+];
 const bloodPressureData = [
   {
     'date': '2019-10-25T04:10:51.530Z',
